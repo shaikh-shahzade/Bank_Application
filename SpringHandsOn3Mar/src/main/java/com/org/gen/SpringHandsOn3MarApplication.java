f8609797d40c28790e0fe99454ba130fe2c2df01
Anuj <anuj.mca.nitrr.2019@gmail.com>
2022-03-03 23:32:01 +0530

    Spring HandsOn 3 March

---
+++ SpringHandsOn3Mar/src/main/java/com/org/gen/SpringHandsOn3MarApplication.java
@@ -0,0 +1,15 @@
+package com.org.gen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHandsOn3MarApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(SpringHandsOn3MarApplication.class, args);
+
+	}
+
+}
